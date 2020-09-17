1c03aa880f432a808d96f62ba958469a3c44ab37
lee3072 <lee3072@purdue.edu>
2020-09-17 15:01:44 -0400

    tf_net first submit

--- lab0.py
+++ lab0.py
@@ -14,7 +14,7 @@ np.random.seed(1618)
 tf.random.set_seed(1618)
 
 # Disable some troublesome logging.
-# tf.logging.set_verbosity(tf.logging.ERROR)   # Uncomment for TF1.
+# tf-.logging.set_verbosity(tf.logging.ERROR)   # Uncomment for TF1.
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 # Information on dataset.
@@ -22,9 +22,9 @@ NUM_CLASSES = 10
 IMAGE_SIZE = 784
 
 # Use these to set the algorithm to use.
-ALGORITHM = "guesser"
+# ALGORITHM = "guesser"
 #ALGORITHM = "custom_net"
-#ALGORITHM = "tf_net"
+ALGORITHM = "tf_net"
 
 
 
@@ -55,12 +55,25 @@ class NeuralNetwork_2Layer():
 
     # Training with backpropagation.
     def train(self, xVals, yVals, epochs = 100000, minibatches = True, mbs = 100):
-        # layer1, layer2 = self.__forward(xVals)
-        # layer2_delta = (yVals - layer2) * self.__sigmoidDerivative(layer2)
-        # layer1_delta = layer2_delta.dot(self.W2.T) * self.__sigmoidDerivative(layer1)
-        # self.W1 += layer1.T.dot(layer2_delta)
-        # self.W2 += xVals.T.dot(layer1_delta)
-        pass                                   #TODO: Implement backprop. allow minibatches. mbs should specify the size of each minibatch.
+        #Done: Implement backprop. allow minibatches. mbs should specify the size of each minibatch.
+        #reference: https://thispointer.com/python-use-of-yield-keyword-generators-explained-with-examples/
+        #reference: https://www.tensorflow.org/guide/eager
+        xValsBatchGen = self.__batchGenerator(xVals,mbs)
+        yValsBatchGen = self.__batchGenerator(yVals,mbs)
+        while True:
+            try:
+                xValsBatch = next(xValsBatchGen)
+                yValsBatch = next(yValsBatchGen)
+                for i in range(epochs):
+                    layer1, layer2 = self.__forward(xValsBatch)
+                    layer2_delta = (yValsBatch - layer2) * self.__sigmoidDerivative(layer2)
+                    layer1_delta = layer2_delta.dot(self.W2.T) * self.__sigmoidDerivative(layer1)
+                    self.W1 += layer1.T.dot(layer2_delta) * self.lr
+                    self.W2 += xVals.T.dot(layer1_delta) * self.lr    
+            except StopIteration:
+                break
+        
+                                          
 
     # Forward pass.
     def __forward(self, input):
@@ -100,7 +113,11 @@ def getRawData():
 
 
 def preprocessData(raw):
-    ((xTrain, yTrain), (xTest, yTest)) = raw            #TODO: Add range reduction here (0-255 ==> 0.0-1.0).
+    ((xTrain, yTrain), (xTest, yTest)) = raw            
+    
+    #Done: Add range reduction here (0-255 ==> 0.0-1.0).
+    xTrain, xTest = xTrain/255.0, xTest/255.0   #reference https://colab.research.google.com/github/tensorflow/docs/blob/master/site/en/tutorials/quickstart/beginner.ipynb?hl=ko#scrollTo=7FP5258xjs-v
+
     yTrainP = to_categorical(yTrain, NUM_CLASSES)
     yTestP = to_categorical(yTest, NUM_CLASSES)
     print("New shape of xTrain dataset: %s." % str(xTrain.shape))
@@ -120,9 +137,11 @@ def trainModel(data):
         print("Not yet implemented.")                   #TODO: Write code to build and train your custon neural net.
         return None
     elif ALGORITHM == "tf_net":
-        print("Building and training TF_NN.")
-        print("Not yet implemented.")                   #TODO: Write code to build and train your keras neural net.
-        return None
+        #Done: Write code to build and train your keras neural net.
+        model = tf.keras.models.Sequential([tf.keras.layers.Flatten(),tf.keras.layers.Dense(512,activation=tf.nn.relu),tf.keras.layers.Dense(10,activation=tf.nn.softmax)])
+        model.compile(optimizer='adam',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+        model.fit(xTrain,np.argmax(yTrain, axis=1),epochs=5) # Reverse the to_categorical to perform fit, reference: https://github.com/keras-team/keras/issues/4981
+        return model
     else:
         raise ValueError("Algorithm not recognized.")
 
@@ -136,9 +155,10 @@ def runModel(data, model):
         print("Not yet implemented.")                   #TODO: Write code to run your custon neural net.
         return None
     elif ALGORITHM == "tf_net":
-        print("Testing TF_NN.")
-        print("Not yet implemented.")                   #TODO: Write code to run your keras neural net.
-        return None
+        #Done: Write code to run your keras neural net.
+        # predicted data will be an array representing the predicted number,
+        # convert it to a single number to assign to a category
+        return to_categorical(np.argmax(model.predict(data), axis=1), NUM_CLASSES)
     else:
         raise ValueError("Algorithm not recognized.")
 
